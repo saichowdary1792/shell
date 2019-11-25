4cc10fac99213e4d5e9d06b7e5c6dabad3f248f5
saichowdary1792 <58160313+saichowdary1792@users.noreply.github.com>
2019-11-25 11:31:39 +0530

    updated casing.sh

--- casing.sh
+++ casing.sh
@@ -3,9 +3,6 @@ echo -e "enter character: \c"
 read p
 case $p in [a-z]) echo $p is small;;
 	[A-Z]) echo $p is large ;;
-	[0-9]) echo $p is number ;;
-	?) echo $p is special ;;
-	*)echo $p is unknown;;
 esac
 
 
